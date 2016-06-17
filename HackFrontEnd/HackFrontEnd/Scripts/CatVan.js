038b7d3a7afced703e4a418a57ce34906e7806af
Nate Christiansen <nathan.christiansen@erieinsurance.com>
2016-06-17 17:14:06 -0400

    extra stuff

---
+++ HackFrontEnd/HackFrontEnd/Scripts/CatVan.js
@@ -0,0 +1,21 @@
+﻿function displayMap() {
+    $('#map').css('display', 'block');
+    var zip = $('#enterbox').val();
+    $.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + zip, function(data) {
+        initialize(data.results[0].geometry.location);
+    });
+}
+function initialize(loc) {
+    var mapProp = {
+        center: new google.maps.LatLng(loc.lat, loc.lng),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    new google.maps.Marker({
+        position: { lat: loc.lat+(Math.random()-.5), lng: loc.lng+(Math.random()-.5) },
+        map: map,
+        title: '1'
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
