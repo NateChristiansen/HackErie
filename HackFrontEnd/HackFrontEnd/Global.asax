6d7604710036fab86f18df05be37fc2b4a389ab6
Nate Christiansen <nathan.christiansen@erieinsurance.com>
2016-06-17 08:46:56 -0400

    got socket connected, started webapp

---
+++ HackFrontEnd/HackFrontEnd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackFrontEnd.MvcApplication" Language="C#" %>
--- WeatherBackend/WeatherBackend/Program.cs
+++ WeatherBackend/WeatherBackend/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WeatherBackend
+﻿namespace WeatherBackend
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            new BackendServer();
         }
     }
 }
