701acf49a0614f602fd712744b443312cda28b8e
Nate Christiansen <nathan.christiansen@erieinsurance.com>
2016-06-17 09:04:59 -0400

    more work

---
+++ HackFrontEnd/HackFrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackFrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult PotentialCat()
+        {
+            var fromAddress = new MailAddress("from@gmail.com", "From Name");
+            var toAddress = new MailAddress("to@example.com", "To Name");
+            const string fromPassword = "fromPassword";
+            const string subject = "Subject";
+            const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+            return Json("Hit the deck!", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
